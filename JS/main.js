63d6fa1cb6f671469f6241c29395c4ed24b4a37f
Soribel SB <soribelsantos0514@gmail.com>
2023-05-05 13:40:26 -0400

    Feat localStorage task
    Delete localStorage task

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,12 @@ import { weatherLocal } from "./weather.js";
 let ul = document.querySelector('.task-list');
 let title = document.getElementById("title");
 let content = document.getElementById("content");
-let guests = document.getElementById("guests");
 let gretting = document.querySelector(".gretting");
+let sendBtn = document.getElementById("send-btn");
+let empty = document.getElementById("empty");
+let listTask = [];
+
+getStorageTask();
 
 // Saludo de bienvenida
 let time = new Date();
@@ -22,54 +26,80 @@ else {
   gretting.textContent = `Hi, welcome 👋🏽`;
 }
 
-function creatorTasks(event) {
+sendBtn.addEventListener("click",event => {
   event.preventDefault();
+  let task = document.createElement
+  ("li");
+  task.classList.add("task");
 
   if(title.value !== ''){
+    task.innerHTML = `
+      <h2>${title.value}</h2>
+      <p>${content.value}</p>
+    `;    
+    task.appendChild(deleteBtn())
+    ul.appendChild(task);
 
-    let li = document.createElement('li');
-    li.classList.add("task");
-    let h2Title = document.createElement('h2');
-    let pContent = document.createElement('p');
-    let pGuests = document.createElement('div');
-
-    h2Title.textContent = title.value;
-    pContent.textContent = content.value;
-    pGuests.textContent = guests.value;
-
-    let iconEdit = document.createElement('i');
-    iconEdit.classList.add("bxs-edit-alt");
-    
-    let iconCheck = document.createElement('i');
-    iconCheck.classList.add("bxs-check-circle");
-
-    li.appendChild(h2Title);
-    li.appendChild(pContent);
-    li.appendChild(pGuests);
-    li.appendChild(DeleteBtn());
-    ul.appendChild(li);
+    let taskObj = {
+      title : title.value,
+      content : content.value
+    }
+    listTask.push(taskObj);
+    localStorage.setItem("listTask",JSON.stringify(listTask));
 
     title.value = '';
     content.value= '';
-    guests.value= '';
+    empty.style.display = 'none';
   }
-}
-function DeleteBtn(){
+})
+function deleteBtn(){
     let iconTrash = document.createElement('i');
     iconTrash.classList.add("bx");
     iconTrash.classList.add("bxs-trash-alt");
 
     iconTrash.addEventListener('click',(e) => {
         const item = e.target.parentElement;
-        ul.removeChild(item)       
-    })
+        ul.removeChild(item);
 
+        let tasks = JSON.parse(localStorage.getItem("listTask"));
+        // Eliminar elementos del storage
+        for (let i = 0; i < tasks.length; i++) {
+          let taskValue = item.children[0].innerHTML
+          if (taskValue === tasks[i].title) {
+              tasks.splice(i,1);
+              localStorage.setItem("listTask",JSON.stringify(tasks));
+          }
+        }
+
+        const items = ul.childElementCount;
+        if(items === 0){
+          empty.style.display = 'block';
+          localStorage.clear();
+      }
+    })
     return iconTrash;
-    
+}
+function getStorageTask(){
+  let tasks = JSON.parse(localStorage.getItem("listTask"));
+
+  if (tasks !== null) {
+    tasks.forEach(taskValue => {
+      localStorage.getItem(taskValue);
+
+      let task = document.createElement
+      ("li");
+      task.classList.add("task");
+      task.innerHTML = `
+        <h2>${taskValue.title}</h2>
+        <p>${taskValue.content}</p>
+      `;    
+      task.appendChild(deleteBtn())
+      ul.appendChild(task)
+    });
+  }
 }
 // TASK INTERACTIVIDAD
 let addBtn = document.querySelector(".bxs-plus-circle");
-let sendBtn = document.getElementById("send-btn");
 let closeBtn = document.querySelector(".bxs-x-circle");
 
 addBtn.addEventListener("click",()=>{
